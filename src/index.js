102944f0f822cb686403774d580ef70df6dd77c4
MakeZzz <maxim1080@gmail.com>
2018-03-05 18:56:13 +0300

    master/feacher: Zeros-advanced

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 module.exports = function getZerosCount(number, base) {
-  var number, base, i = 2
-  var counter = 0
-  for (i = 2; i <= base; i++) {
-  if (base % i == 0) {
-  var m = 0
-  while (base % i == 0) {
-  m = i;
-  base /= i;
-  }
-  while (number > 0) {
-    number = Math.floor(number/m);
-    counter += number;
-  }
-  return counter;
+var a = [];
+var counter = 0;
+var base2 = base;
+var Zeros = Infinity;
+  for (i = 2; i <= base2; i++) {
+      if (base2 % i == 0) {
+        var m = 0
+        while (base2 % i == 0) {
+          m++;
+          base2 /= i;
+        }
+var result = 0;
+var b = i;
+  do {
+    counter = Math.floor(number / b);
+    b *= i;
+    result += counter;
+}
+while (counter != 0) ;
+
+var testValue = Math.floor (result / m);
+  if (testValue < Zeros) {
+    Zeros = testValue;
 }
 }
+}
+return Zeros;
 }
